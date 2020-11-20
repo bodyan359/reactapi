ecb30766d719e9c5dbcf80eee6e51d2cde84e3ee
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2020-11-20 11:44:58 +0100

    https://jsonplaceholder.typicode.com/users add fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 
+import React, { Component } from 'react'
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+    
+  }
+
+  render() {
+
+    var { isLoaded, items} = this.state;
+    if (!isLoaded){
+      return<div>Loading...</div>;
+    }else{
+
+    
+    return (
+      <div className="App">
+      Data has been loaded.
+
+        <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                {item.name} | {item.email}
+              </li>  
+            ))}
+        </ul>
+
+      </div>
+    );
+    }
+  }
 }
 
 export default App;
